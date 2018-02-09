56e38acd641a6c02a7dd0e62c70d2286524f0a9b
Lenovo <Lenovo@DESKTOP-JFLSFPK>
2018-02-09 16:13:45 +0100

    first changes.

---
+++ SimpleDLL/SimpleDLL/SimpleDLL.cpp
@@ -0,0 +1,10 @@
+#include "SimpleDLL.h"
+#include <iostream>
+
+using namespace std;
+using namespace nmspace;
+
+void myclass::Crap()
+{
+	cout << "I'm called within a crap!" << endl;
+}
---
+++ SimpleDLL/SimpleDLL/SimpleDLL.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace nmspace
+{
+	class myclass
+	{
+	public:
+		static __declspec(dllexport) void Crap();
+	};
+
+}
